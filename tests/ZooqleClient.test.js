f65462daa4e3192815785d822b0070e3c8f27285
Naughty Doge <naughty-doge@protonmail.com>
2018-05-15 00:51:37 +0700

    Update tests

--- tests/ZooqleClient.test.js
+++ tests/ZooqleClient.test.js
@@ -4,6 +4,7 @@ import ZooqleClient from '../src/ZooqleClient'
 
 const MOVIE_PAGE_AUTHORIZED = readFileSync(`${__dirname}/moviePageAuthorized.html`, 'utf8')
 const MOVIE_PAGE_GUEST = readFileSync(`${__dirname}/moviePageGuest.html`, 'utf8')
+const SHOW_RESPONSE = readFileSync(`${__dirname}/showResponse.html`, 'utf8')
 
 
 describe('ZooqleClient', () => {
@@ -13,28 +14,41 @@ describe('ZooqleClient', () => {
     client = new ZooqleClient({ userName: 'foo', password: 'bar' })
   })
 
-  describe('#_extractTorrentsFromMoviePage()', () => {
+  describe('#_extractTorrentsFromPage()', () => {
     test('extracts torrents with magnet links from the sample authorized movie page', () => {
-      let results = client._extractTorrentsFromMoviePage(MOVIE_PAGE_AUTHORIZED)
+      let results = client._extractTorrentsFromPage(MOVIE_PAGE_AUTHORIZED)
 
       expect(results).toHaveLength(25)
       results.forEach((item) => {
         expect(item.category).toBeTruthy()
-        expect(item.url).toBeTruthy()
         expect(item.magnetLink).toBeTruthy()
         expect(item.seeders).toBeGreaterThanOrEqual(0)
+        expect(item.languages).toBeInstanceOf(Array)
       })
     })
 
     test('extracts torrents without magnet links from the sample guest movie page', () => {
-      let results = client._extractTorrentsFromMoviePage(MOVIE_PAGE_GUEST)
+      let results = client._extractTorrentsFromPage(MOVIE_PAGE_GUEST)
 
       expect(results).toHaveLength(17)
       results.forEach((item) => {
         expect(item.category).toBeTruthy()
-        expect(item.url).toBeTruthy()
         expect(item.magnetLink).toBeFalsy()
         expect(item.seeders).toBeGreaterThanOrEqual(0)
+        expect(item.languages).toBeInstanceOf(Array)
+      })
+    })
+
+    test('extracts torrents with magnet links from the sample show response', () => {
+      let results = client._extractTorrentsFromPage(SHOW_RESPONSE)
+
+      expect(results).toHaveLength(2)
+      results.forEach((item) => {
+        expect(item.category).toBe('Std')
+        expect(item.magnetLink).toBeTruthy()
+        expect(item.seeders).toBeGreaterThanOrEqual(0)
+        expect(item.audio).toBe('2.0')
+        expect(item.languages).toEqual([])
       })
     })
   })

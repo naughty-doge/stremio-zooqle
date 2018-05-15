175a881fc9a84a53178ed25a5b0a3a552ca6f151
Naughty Doge <naughty-doge@protonmail.com>
2018-05-15 17:53:45 +0700

    Add tests for the addon itself

---
+++ tests/index.test.js
@@ -0,0 +1,123 @@
+/* eslint-disable no-console */
+
+import { get } from 'http'
+import { Client as AddonClient } from 'stremio-addons'
+
+
+jest.mock('../src/ZooqleClient')
+
+// Prevent the addon from printing
+// eslint-disable-next-line no-unused-vars
+let log = console.log
+console.log = () => {}
+console.error = () => {}
+
+function reset() {
+  jest.resetModules()
+
+  delete process.env.STREMIO_ZOOQLE_ID
+  delete process.env.STREMIO_ZOOQLE_ENDPOINT
+  delete process.env.STREMIO_ZOOQLE_PORT
+  delete process.env.STREMIO_ZOOQLE_PROXY
+  delete process.env.STREMIO_ZOOQLE_CACHE
+  delete process.env.STREMIO_ZOOQLE_EMAIL
+  delete process.env.STREMIO_ZOOQLE_USERNAME
+  delete process.env.STREMIO_ZOOQLE_PASSWORD
+  delete process.env.NODE_ENV
+}
+
+function initAddon() {
+  return {
+    start() {
+      // eslint-disable-next-line global-require
+      this.server = require('../src/index').default
+
+      return new Promise((resolve, reject) => {
+        this.server.once('listening', () => resolve(this))
+        this.server.once('error', (err) => {
+          reject(err)
+          this.stop()
+        })
+      })
+    },
+
+    stop() {
+      if (!this.server) {
+        return Promise.resolve(this)
+      }
+
+      let stopPromise = new Promise((resolve) => {
+        this.server.once('close', () => resolve(this))
+      })
+      this.server.close()
+      return stopPromise
+    },
+  }
+}
+
+describe('Addon', () => {
+  let addonClient
+  let addon
+
+  beforeAll(() => {
+    addonClient = new AddonClient()
+    addonClient.add('http://localhost')
+  })
+
+  beforeEach(() => {
+    reset()
+    addon = initAddon()
+  })
+
+  afterEach(() => {
+    return addon.stop()
+  })
+
+  test('When a port is not specified, starts a web server on port 80', async () => {
+    await addon.start()
+    expect(addon.server.address().port).toBe(80)
+  })
+
+  test('When a port is specified, starts a web server on it', async () => {
+    process.env.STREMIO_ZOOQLE_PORT = '9028'
+    await addon.start()
+    expect(addon.server.address().port).toBe(9028)
+  })
+
+  test('stream.find is implemented', async (done) => {
+    await addon.start()
+
+    addonClient.stream.find({}, (err) => {
+      err ? done.fail(err) : done()
+    })
+  })
+
+  test('The main page is accessible', async () => {
+    await addon.start()
+    let res = await new Promise((resolve) => {
+      get('http://localhost', resolve)
+    })
+    expect(res.statusCode).toBe(200)
+  })
+
+  test('The static files are accessible', async () => {
+    await addon.start()
+    let staticFiles = [
+      'logo-white.png',
+    ]
+    let promises = staticFiles.map((file) => {
+      return new Promise((resolve) => {
+        get(`http://localhost/${file}`, resolve)
+      })
+    })
+    let responses = await Promise.all(promises)
+
+    responses.forEach((res) => {
+      // Requests to non-existent files return the landing page,
+      // so we check that the response is not HTML
+      let contentType = res.headers['content-type'].split(';')[0]
+      expect(contentType).not.toBe('text/html')
+      expect(res.statusCode).toBe(200)
+    })
+  })
+})
